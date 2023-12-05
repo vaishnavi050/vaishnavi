a4990eb8c8e34e209cb0977c3d848726e49a9ef7
V0508 <142962560+V0508@users.noreply.github.com>
2023-12-05 12:04:49 +0530

    share readwritedata

---
+++ src/test/java/readwritedata1/writedata.java
@@ -0,0 +1,22 @@
+package readwritedata1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class writedata {
+	
+	public static void main (String []args) throws FileNotFoundException
+	{
+		FileInputStream fis =new FileInputStream ("");
+		
+	}
+
+}
+
+
+
+
+
+
+
+
